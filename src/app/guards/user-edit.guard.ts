c857f227e744c87c3d5a98282e8e2f3459065185
Amador Santiago <amadorsantiago368@gmail.com>
2026-08-21 00:17:06 -0600

    user edit guard applied

--- src/app/guards/user-edit.guard.ts
+++ src/app/guards/user-edit.guard.ts
@@ -1,5 +1,17 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
 
 export const userEditGuard: CanActivateFn = (route, state) => {
+
+  const router = inject(Router);
+  const id = Number(route.paramMap.get('id'));
+
+  if (!id || isNaN(id)) {
+    router.navigate(['/users']);
+    return false;
+  }
+  if (Number(id) > 10) {
+    return router.createUrlTree(['/users']);
+  }
   return true;
 };
